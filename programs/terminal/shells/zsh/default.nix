1462033b0dbfac9c4716dad34fd7deeb765d31f7
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2025-12-31 10:21:09 +0100

    Added zsh suffix and global shell aliases

--- programs/terminal/shells/zsh/default.nix
+++ programs/terminal/shells/zsh/default.nix
@@ -12,9 +12,22 @@
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
     ];
-
+    
+    # PowerLevel10k config, and suffix aliases
     initContent = ''
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+      alias -s md="bat"
+      alias -s mov="open"
+      alias -s png="open"
+      alias -s mp4="open"
+      alias -s {yml,yaml}="bat -l yaml"
+      alias -s json="fx"
+      alias -s {js,ts,jsx,tsx,py}="code"
+      alias -s lua="nvim"
+      alias -s go="goland"
+      alias -s {kt,java}="idea"
+      alias -s nix="code"
     '';
 
     oh-my-zsh = {
@@ -46,6 +59,15 @@
       v = "xclip -o";
       sc = "shellcheck";
       da = "direnv allow";
+    };
+
+    shellGlobalAliases = {
+      NE = "2>/dev/null";
+      ND = ">/dev/null";
+      NUL = ">/dev/null 2>1";
+      JQ = "| jq";
+      C = "| wl-copy";
+      L="| less";
     };
   };
 
